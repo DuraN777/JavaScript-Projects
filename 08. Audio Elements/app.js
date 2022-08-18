1b9b647ae330791822576ae4f9cdeabf99221a2a
Djura Neskovic <neskovic991@gmail.com>
2022-08-18 13:00:05 +0200

    first commit

---
+++ 08. Audio Elements/app.js
@@ -0,0 +1,30 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+
+sounds.forEach(sound => {
+  // Create button elements wih the elements from sounds array
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+
+  // Populate inner text of each button with sounds elements
+  btn.innerText = sound;
+
+  // PLay sound on click
+  btn.addEventListener('click', () => {
+    stopAudio();
+    document.getElementById(sound).play();
+  })
+  // Inserting them into HTML in the id='buttons' DIV
+  document.getElementById('buttons').append(btn);
+})
+
+
+// audio elements dont have STOP, we need to pause and reset playtime to 0
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/currentTime
+function stopAudio() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  })
+}
